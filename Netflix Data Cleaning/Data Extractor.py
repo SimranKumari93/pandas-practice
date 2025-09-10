0c1e0f3e01bd8fa0ad637819a8cfbac2aa62d55d
simran <simrankumaribodhgaya93@gmail.com>
2025-09-10 22:46:31 +0530

    netflix-data-cleaning

---
+++ Netflix Data Cleaning/Data Extractor.py
@@ -0,0 +1,6 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("ariyoomotade/netflix-data-cleaning-analysis-and-visualization")
+
+print("Path to dataset files:", path)
